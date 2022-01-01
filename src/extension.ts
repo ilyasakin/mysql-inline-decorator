8c8b096f946a5bbbc8566400fd25dfe95189f213
Ilyas Akin <ashnwor@gmail.com>
2022-01-01 16:14:44 +0300

    Move functions out of the activate() function

--- src/extension.ts
+++ src/extension.ts
@@ -3,14 +3,124 @@ import decorations from './decorations';
 import { functions, keywords } from './dictionary';
 
 const MEASURE_SPEED: boolean = false;
+const FILE_EXTENSION_WHITELIST: string[] = ['js', 'ts'];
+
+let activeEditor: vscode.TextEditor | undefined =
+  vscode.window.activeTextEditor;
+let currentFileExtension: string = '';
+
+const findInMatch = (
+  iMatch: RegExpExecArray,
+  dictionary: string[],
+  iAdjustment?: number
+): vscode.Range[] => {
+  if (MEASURE_SPEED) {
+    console.time('Find In Match Completion Time');
+  }
+
+  const matches: vscode.Range[] = [];
+  const template: string = iMatch[0];
+
+  dictionary.forEach((keyword: string): void => {
+    if (!template.includes(keyword)) {
+      return;
+    }
+
+    const matchesInTemplate: RegExpMatchArray[] = [
+      ...template.matchAll(new RegExp(`(?:\\b|^)(${keyword})(?=\\b|$)`, 'gi')),
+    ];
+
+    matchesInTemplate.forEach((match: RegExpMatchArray) => {
+      if (!activeEditor) {
+        return;
+      }
+
+      const startPos: vscode.Position = activeEditor.document.positionAt(
+        match.index! + iMatch.index
+      );
+
+      const endPos: vscode.Position = activeEditor.document.positionAt(
+        match.index! + match[0].length + iMatch.index + Number(iAdjustment || 0)
+      );
+
+      matches.push(new vscode.Range(startPos, endPos));
+    });
+  });
+
+  if (MEASURE_SPEED) {
+    console.timeEnd('Find In Match Completion Time');
+  }
+
+  return matches;
+};
+
+const updateDecorations = (): void => {
+  if (
+    !activeEditor ||
+    !FILE_EXTENSION_WHITELIST.includes(currentFileExtension)
+  ) {
+    return;
+  }
+
+  const templateStringRegex: RegExp = /`([^`]|(\`))*?(?<!\\)`/gm;
+  const text: string = activeEditor.document.getText();
+
+  let match: RegExpExecArray | null;
+  const wKeywords: vscode.Range[] = [];
+  const wFunctions: vscode.Range[] = [];
+  const wStrings: vscode.Range[] = [];
+  const wParams: vscode.Range[] = [];
+  const wNone: vscode.Range[] = [];
+
+  while ((match = templateStringRegex.exec(text))) {
+    const wKeywordFound: vscode.Range[] = findInMatch(match, keywords);
+    const wFunctionFound: vscode.Range[] = findInMatch(match, functions, -1);
+
+    //at least 2 keywords found
+    if (wKeywordFound.length + wFunctionFound.length > 1) {
+      const startPos: vscode.Position = activeEditor.document.positionAt(
+        match.index
+      );
+      const endPos: vscode.Position = activeEditor.document.positionAt(
+        match.index + match[0].length
+      );
+      const decoration: vscode.Range = new vscode.Range(startPos, endPos);
+      const wStringFound: vscode.Range[] = findInMatch(match, keywords);
+      const wParamsFound: vscode.Range[] = findInMatch(match, functions);
+
+      wKeywords.push(...wKeywordFound);
+      wFunctions.push(...wFunctionFound);
+      wStrings.push(...wStringFound);
+      wParams.push(...wParamsFound);
+      wNone.push(decoration);
+    }
+  }
+
+  activeEditor.setDecorations(decorations.none, wNone);
+  activeEditor.setDecorations(decorations.param, wParams);
+  activeEditor.setDecorations(decorations.string, wStrings);
+  activeEditor.setDecorations(decorations.keyword, wKeywords);
+  activeEditor.setDecorations(decorations.function, wFunctions);
+};
+
+let timeout: NodeJS.Timeout | null = null;
+const triggerUpdateDecorations = (): void => {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+
+  timeout = setTimeout(updateDecorations, 150);
+};
+
+const getFileExtension = (activeEditor: vscode.TextEditor): string => {
+  return activeEditor.document.fileName.substring(
+    activeEditor.document.fileName.lastIndexOf('.') + 1,
+    activeEditor.document.fileName.length
+  );
+};
 
 // this method is called when vs code is activated
-export function activate(context: vscode.ExtensionContext): void {
-  const FILE_EXTENSION_WHITELIST: string[] = ['js', 'ts'];
-  let activeEditor: vscode.TextEditor | undefined =
-    vscode.window.activeTextEditor;
-  let currentFileExtension: string = '';
-
+export const activate = (context: vscode.ExtensionContext): void => {
   if (activeEditor) {
     currentFileExtension = getFileExtension(activeEditor);
     triggerUpdateDecorations();
@@ -37,119 +147,4 @@ export function activate(context: vscode.ExtensionContext): void {
     null,
     context.subscriptions
   );
-
-  var timeout: NodeJS.Timeout | null = null;
-  function triggerUpdateDecorations(): void {
-    if (timeout) {
-      clearTimeout(timeout);
-    }
-
-    timeout = setTimeout(updateDecorations, 150);
-  }
-
-  function getFileExtension(activeEditor: vscode.TextEditor): string {
-    return activeEditor.document.fileName.substring(
-      activeEditor.document.fileName.lastIndexOf('.') + 1,
-      activeEditor.document.fileName.length
-    );
-  }
-
-  function updateDecorations(): void {
-    if (
-      !activeEditor ||
-      !FILE_EXTENSION_WHITELIST.includes(currentFileExtension)
-    ) {
-      return;
-    }
-
-    const templateStringRegex: RegExp = /`([^`]|(\`))*?(?<!\\)`/gm;
-    const text: string = activeEditor.document.getText();
-
-    let match: RegExpExecArray | null;
-    const wKeywords: vscode.Range[] = [];
-    const wFunctions: vscode.Range[] = [];
-    const wStrings: vscode.Range[] = [];
-    const wParams: vscode.Range[] = [];
-    const wNone: vscode.Range[] = [];
-
-    while ((match = templateStringRegex.exec(text))) {
-      const wKeywordFound: vscode.Range[] = findInMatch(match, keywords);
-      const wFunctionFound: vscode.Range[] = findInMatch(match, functions, -1);
-
-      //at least 2 keywords found
-      if (wKeywordFound.length + wFunctionFound.length > 1) {
-        const startPos: vscode.Position = activeEditor.document.positionAt(
-          match.index
-        );
-        const endPos: vscode.Position = activeEditor.document.positionAt(
-          match.index + match[0].length
-        );
-        const decoration: vscode.Range = new vscode.Range(startPos, endPos);
-        const wStringFound: vscode.Range[] = findInMatch(match, keywords);
-        const wParamsFound: vscode.Range[] = findInMatch(match, functions);
-
-        wKeywords.push(...wKeywordFound);
-        wFunctions.push(...wFunctionFound);
-        wStrings.push(...wStringFound);
-        wParams.push(...wParamsFound);
-        wNone.push(decoration);
-      }
-    }
-
-    activeEditor.setDecorations(decorations.none, wNone);
-    activeEditor.setDecorations(decorations.param, wParams);
-    activeEditor.setDecorations(decorations.string, wStrings);
-    activeEditor.setDecorations(decorations.keyword, wKeywords);
-    activeEditor.setDecorations(decorations.function, wFunctions);
-  }
-
-  function findInMatch(
-    iMatch: RegExpExecArray,
-    dictionary: string[],
-    iAdjustment?: number
-  ): vscode.Range[] {
-    if (MEASURE_SPEED) {
-      console.time('Find In Match Completion Time');
-    }
-
-    const matches: vscode.Range[] = [];
-    const template: string = iMatch[0];
-
-    dictionary.forEach((keyword: string): void => {
-      if (!template.includes(keyword)) {
-        return;
-      }
-
-      const matchesInTemplate: RegExpMatchArray[] = [
-        ...template.matchAll(
-          new RegExp(`(?:\\b|^)(${keyword})(?=\\b|$)`, 'gi')
-        ),
-      ];
-
-      matchesInTemplate.forEach((match: RegExpMatchArray) => {
-        if (!activeEditor) {
-          return;
-        }
-
-        const startPos: vscode.Position = activeEditor.document.positionAt(
-          ((match as RegExpMatchArray).index as number) + iMatch.index
-        );
-
-        const endPos: vscode.Position = activeEditor.document.positionAt(
-          ((match as RegExpMatchArray).index as number) +
-            match[0].length +
-            iMatch.index +
-            Number(iAdjustment || 0)
-        );
-
-        matches.push(new vscode.Range(startPos, endPos));
-      });
-    });
-
-    if (MEASURE_SPEED) {
-      console.timeEnd('Find In Match Completion Time');
-    }
-
-    return matches;
-  }
-}
+};
